5b66425c4d3d6903d9b02eb09083553413c70aa2
satoshi hikida <s.hikida@hotmail.co.jp>
2014-08-18 20:58:55 +0900

    RSAの暗号化，復号化

--- src/generateAndVerifyingSignatures/GenSig.java
+++ src/generateAndVerifyingSignatures/GenSig.java
@@ -13,8 +13,11 @@ public class GenSig {
         if (args.length != 1) {
             System.out.println("Usage: GenSig nameOfFileToSign");
         } else try {
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+//            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+//            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            SecureRandom random = new SecureRandom();
+
             keyGen.initialize(1024, random);
 
             KeyPair pair = keyGen.generateKeyPair();
@@ -22,7 +25,8 @@ public class GenSig {
             PublicKey pub = pair.getPublic();
 
             // TODO check this class API doc
-            Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+//            Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+            Signature dsa = Signature.getInstance("SHA1withRSA");
             dsa.initSign(priv);
 
             FileInputStream fis = new FileInputStream(args[0]);
@@ -34,6 +38,17 @@ public class GenSig {
             }
             bufin.close();
             byte[] realSig = dsa.sign();
+
+            /* save the signature in a file */
+            FileOutputStream sigfos = new FileOutputStream("sig");
+            sigfos.write(realSig);
+            sigfos.close();
+
+            /* save the public key in a file */
+            byte[] key = pub.getEncoded();
+            FileOutputStream keyfos = new FileOutputStream("suepk");
+            keyfos.write(key);
+            keyfos.close();
 
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
